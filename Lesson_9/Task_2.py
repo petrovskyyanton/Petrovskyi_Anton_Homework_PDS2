425b17eadad7470c434672db0d1428d2eab0bdce
Anton Petrovskyi <petrovskyyanton@gmail.com>
2022-11-04 15:28:46 +0200

    Commit. Lesson 9

--- Lesson_9/Task_2.py
+++ Lesson_9/Task_2.py
@@ -1,22 +1,17 @@
-import string
-
-
 class TextProcessor:
-    def __init__(self, text):
-        self.text = text
+    def __init__(self):
+        self._punctuation = '.,!?;:*'
 
-    def get_clean_string(self):
-        for i in string.punctuation:
-            if i in self.text:
-                self.text = self.text.replace(i, '')
-        return self.text
+    def __is_punctuation(self, sign):
+       return sign in self._punctuation
 
-    def __is_punctuation(self):
-        for i in self.text:
-            if i in string.punctuation:
-                return True
-            else:
-                return False
+    def get_clean_string(self, text):
+        clean_text = ''
+        for sign in text:
+            if self.__is_punctuation(sign):
+                continue
+            clean_text += sign
+        return clean_text
 
 
 class TextLoader:
@@ -25,11 +20,12 @@ class TextLoader:
         a = input("Text")
         __text_processor = TextProcessor(a)
     def set_clean_text(self):
-        __clean_string = f'{self.__text_processor.get_clean_string()}'
+        clean_string = self.__text_processor.get_clean_string()
+        return clean_string
 
     @property
-    def __clean_string(self):
-        return __clean_string
+    def clean_string(self):
+        return clean_string
 
 
 
