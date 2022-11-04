586f66706bedab01ae4cbadf6997aae970813efb
Anton Petrovskyi <petrovskyyanton@gmail.com>
2022-11-04 13:44:14 +0200

    Commit. Lesson 9

--- Lesson_9/Task_1.py
+++ Lesson_9/Task_1.py
@@ -4,16 +4,19 @@ class MyCar:
         self.color = color
         self.volume_of_engine = volume_of_engine
 
-
     def forward(self):
         return 'Drive forward'
+
     def back(self):
         return 'Drive back'
+
 
 class HisCar(MyCar):
     def __init__(self):
         MyCar.__init__(self)
+
     def right(self):
         return 'Turn right'
+
     def left(self):
         return 'Turn left'
