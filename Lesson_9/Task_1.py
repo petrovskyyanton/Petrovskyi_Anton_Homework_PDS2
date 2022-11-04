baa5c9ed970f984ff580fa5719cf2c5bd3410a8f
Anton Petrovskyi <petrovskyyanton@gmail.com>
2022-11-04 13:42:57 +0200

    Commit. Lesson 9

---
+++ Lesson_9/Task_1.py
@@ -0,0 +1,19 @@
+class MyCar:
+    def __int__(self, mark, color, volume_of_engine):
+        self.mark = mark
+        self.color = color
+        self.volume_of_engine = volume_of_engine
+
+
+    def forward(self):
+        return 'Drive forward'
+    def back(self):
+        return 'Drive back'
+
+class HisCar(MyCar):
+    def __init__(self):
+        MyCar.__init__(self)
+    def right(self):
+        return 'Turn right'
+    def left(self):
+        return 'Turn left'
