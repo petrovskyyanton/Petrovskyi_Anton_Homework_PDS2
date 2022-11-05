c527de04226a66990dec49d40a59e22617942681
Anton Petrovskyi <petrovskyyanton@gmail.com>
2022-11-05 08:21:31 +0200

    Commit. Lesson 9

--- Lesson_9/Task_2.py
+++ Lesson_9/Task_2.py
@@ -1,6 +1,6 @@
 class TextProcessor:
     def __init__(self):
-        self._punctuation = '.,!?;:*'
+        self._punctuation = r'!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
 
     def __is_punctuation(self, sign):
         return sign in self._punctuation
@@ -15,13 +15,41 @@ class TextProcessor:
 
 
 class TextLoader:
-    def __init__(self, __text_processor, __clean_string):
-        self.__text_processor = __text_processor
-        __text_processor = TextProcessor()
-
-    def set_clean_text(self):
-        __clean_string = self.__text_processor.get_clean_string()
+    def __init__(self, text_processor, clean_string=''):
+        self.__text_processor = text_processor
+        self.__clean_string = clean_string
 
     @property
-    def __clean_string(self):
-        return f'This text is cleaned from signs of punctuations:\n{self.__clean_string}'
+    def text_processor(self):
+        return self.__text_processor
+
+    def set_clean_text(self, text):
+        self.clean_string = self.text_processor.get_clean_string(text)
+
+    @property
+    def clean_string(self):
+        return self.__clean_string
+        # f'This text is cleaned from signs of punctuations:\n{self.__clean_string}'
+
+    @clean_string.setter
+    def clean_string(self, text):
+        self.__clean_string = self.text_processor.get_clean_string(text)
+
+
+# class DataInterface:
+#     def __init__(self, text_processor):
+#         self._text_processor = text_processor
+#
+#     def process_texts(self, list_of_strings):
+#         for i in list_of_strings:
+#             print(i)
+
+
+t = input('Text:')
+q = TextProcessor()
+r = TextLoader(q)
+print(q.get_clean_string(t))
+print(r.set_clean_text(t))
+print(r.set_clean_text(t))
+r.clean_string() # str object not callable
+
