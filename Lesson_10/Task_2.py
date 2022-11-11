92189f07f04817ef341227b25d0fb8fd00cb421d
Anton Petrovskyi <petrovskyyanton@gmail.com>
2022-11-11 15:51:08 +0200

    Commit. Lesson 10

--- Lesson_10/Task_2.py
+++ Lesson_10/Task_2.py
@@ -1,4 +1,4 @@
-list_of_smth = [1, 3.0, '3.0', 4, 5, 6, 7, 8, 9]
+list_of_smth = [1, 3.1, 3, 4, 5, 6, 7, 8, 9]
 
 
 def func(list_of_numbers):
--- Lesson_10/Task_3.py
+++ Lesson_10/Task_3.py
@@ -9,17 +9,17 @@ class StrangeError(Exception):
             self.message = None
 
     def __str__(self):
-        return 'Somthing wrong!'
+        return f'Something wrong: {self.message}'
 
 
 def sqrt():
-    try:
-        n = int(input('Enter a positive number:'))
+    try: # Спроба прийняти додатнє число та вирахувати квадратний корінь з нього
+        n = float(input('Enter a positive number:'))
         if n > 0:
-            return math.sqrt(n)
+            return math.sqrt(n) # Якщо число додатнє - повертається квадратний корінь з цього числа
         else:
-            raise StrangeError()
-    except Exception as ex:
+            raise StrangeError('Negative number!') # Якщо число від'ємне - викликається помилка StrangeError
+    except Exception as ex: # Інші типи помилок (зокрема помилки вводу)
         return ex
 
 
