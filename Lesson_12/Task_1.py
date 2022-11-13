71a3c3cc41ba18c5358f163b8d7bf202b24cce5f
Anton Petrovskyi <petrovskyyanton@gmail.com>
2022-11-13 22:03:40 +0200

    Commit. Lesson 12

---
+++ Lesson_12/Task_1.py
@@ -0,0 +1,44 @@
+import concurrent.futures
+import time
+
+
+def factorial(n):
+    res = 1
+    for i in range(1, n + 1):
+        res *= i
+    return res
+
+
+numbers_for_factorials = [i for i in range(1, 11)]
+result = {}
+
+
+# ProcessPoolExecutor
+
+with concurrent.futures.ProcessPoolExecutor() as executor:
+    started_at = time.time()
+    for number, fact in zip(numbers_for_factorials, executor.map(factorial, numbers_for_factorials)):
+        print(f'{number} factorial is: {fact}')
+    t1 = time.time() - started_at
+    result['ProcessPoolExecutor'] = t1
+    print(f'Time in ProcessPoolExecutor: {t1}')
+
+
+# ThreadPoolExecutor
+with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+    started_at = time.time()
+    f = {executor.submit(factorial, number): number for number in numbers_for_factorials}
+    for future in concurrent.futures.as_completed(f):
+        url = f[future]
+        try:
+            data = future.result()
+        except Exception as exc:
+            print(f'{exc}')
+        else:
+            print(f'{data}')
+    t2 = time.time() - started_at
+    result['ThreadPoolExecutor'] = t2
+    print(f'Time in ThreadPoolExecutor: {t2}')
+
+fast = min(result, key=result.get)
+print(f'{fast} is faster')
