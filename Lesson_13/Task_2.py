f23caeac028cdd0593b6ea94fc6e683c4e458667
Anton Petrovskyi <petrovskyyanton@gmail.com>
2022-11-15 11:29:48 +0200

    Commit. Lesson 13

--- Lesson_13/Task_2.py
+++ Lesson_13/Task_2.py
@@ -34,26 +34,30 @@ def heapify(nums, heap_size, root_index):
         heapify(nums, heap_size, largest)
 
 
-def heap_sort(nums, number_of_iteraton):
+def heap_sort(nums):
+    n = len(nums)
+
+    for i in range(n, -1, -1):
+        heapify(nums, n, i)
+
+    for i in range(n - 1, 0, -1):
+        nums[i], nums[0] = nums[0], nums[i]
+        heapify(nums, i, 0)
+
+
+def time_of_sorting(nums, number_of_iteration):
     cur_time = time.time()
-    for i in range(number_of_iteraton):
-        n = len(nums)
-
-        for i in range(n, -1, -1):
-            heapify(nums, n, i)
-
-        for i in range(n - 1, 0, -1):
-            nums[i], nums[0] = nums[0], nums[i]
-            heapify(nums, i, 0)
-    return (time.time() - cur_time)/number_of_iteraton
+    for i in range(number_of_iteration):
+        heap_sort(nums)
+    return (time.time() - cur_time) / number_of_iteration
 
 
 copy_my_list_1 = int_list
 copy_my_list_2 = float_list
 copy_my_list_3 = str_list
 iteration_number = int(input("Number of iteration:"))
-t1 = heap_sort(copy_my_list_1, iteration_number)
-t2 = heap_sort(copy_my_list_2, iteration_number)
-t3 = heap_sort(copy_my_list_3, iteration_number)
+t1 = time_of_sorting(copy_my_list_1, iteration_number)
+t2 = time_of_sorting(copy_my_list_2, iteration_number)
+t3 = time_of_sorting(copy_my_list_3, iteration_number)
 print("Shell Sort:", copy_my_list_1, copy_my_list_2, copy_my_list_3)
 print(f'Time for sorting integers: {t1}\nTime for sorting floats: {t2}\nTime for sorting words: {t3}')
