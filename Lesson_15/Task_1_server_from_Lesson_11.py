005ce866f9d9958d2b3f03753e1eec75b28b4e9c
Anton Petrovskyi <petrovskyyanton@gmail.com>
2022-12-01 11:16:54 +0200

    Commit. Lesson 15

--- Lesson_15/Task_1_server_from_Lesson_11.py
+++ Lesson_15/Task_1_server_from_Lesson_11.py
@@ -6,25 +6,28 @@ module_logger = logging.getLogger("log_from.Task_1_server_from_Lesson_11")
 
 
 def server():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(('', 55000))
-    sock.listen(10)
-    print('Server is running, please, press ctrl+c to stop')
-    while True:
-        conn, addr = sock.accept()
-        print('connected:', addr)
-        data = conn.recv(1024)
-        if data.decode() == 'Warning':
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind(('', 55000))
+        sock.listen(10)
+        print('Server is running, please, press ctrl+c to stop')
+        while True:
+            conn, addr = sock.accept()
+            print('connected:', addr)
             logger = logging.getLogger("log_from.Task_1_server_from_Lesson_11")
-            logger.warning(f'Warning! Something wrong!')
-        logger = logging.getLogger("log_from.Task_1_server_from_Lesson_11")
-        logger.info(f'Message "{data.decode()}" is received from client')
-        print(str(data.decode()))
-        message = input('Enter message:')
-        if message == 'Error':
+            logger.info(f'Connected to {addr}')
+            data = conn.recv(1024)
             logger = logging.getLogger("log_from.Task_1_server_from_Lesson_11")
-            logger.error(f'Error detected')
+            logger.info(f'Message "{data.decode()}" is received from client')
+            print(str(data.decode()))
+            message = input('Enter message:')
+            conn.send(bytes(message, encoding='UTF-8'))
+            logger = logging.getLogger("log_from.Task_1_server_from_Lesson_11")
+            logger.info(f'Message "{message}" is send from server')
+    except Exception as ex:
         logger = logging.getLogger("log_from.Task_1_server_from_Lesson_11")
-        logger.info(f'Message "{message}" is send from server')
-        conn.send(bytes(message, encoding='UTF-8'))
+        logger.error(f'Error detected: {ex}')
+        return f'Error detected: {ex}'
+
+
 
